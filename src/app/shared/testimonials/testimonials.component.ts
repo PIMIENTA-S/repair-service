f7c1c564a19db01c0e076c5119f52b4439f657bf
Angell Pimienta's <80433456+PIMIENTA-S@users.noreply.github.com>
2024-07-03 23:35:04 -0500

    feat: update .ts for testimonials

--- src/app/shared/testimonials/testimonials.component.ts
+++ src/app/shared/testimonials/testimonials.component.ts
@@ -9,4 +9,31 @@ import { Component } from '@angular/core';
 export class TestimonialsComponent {
   icon = icon;
 
+  comments = [
+    {
+      img: 'assets/comments/c-1.jpg',
+      name: 'David Sanchez',
+      username: 'sanchez1vid',
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores quod incidunt tempore cupiditate delectus perferendis fugit culpa optio! Reiciendis qui possimus maiores magni eius, inventore ipsa soluta odit eaque ullam.'
+    },
+    {
+      img: 'assets/comments/c-1.jpg',
+      name: 'David Sanchez',
+      username: 'sanchez1vid',
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores quod incidunt tempore cupiditate delectus perferendis fugit culpa optio! Reiciendis qui possimus maiores magni eius, inventore ipsa soluta odit eaque ullam.'
+    },
+    {
+      img: 'assets/comments/c-1.jpg',
+      name: 'David Sanchez',
+      username: 'sanchez1vid',
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores quod incidunt tempore cupiditate delectus perferendis fugit culpa optio! Reiciendis qui possimus maiores magni eius, inventore ipsa soluta odit eaque ullam.'
+    },
+    {
+      img: 'assets/comments/c-1.jpg',
+      name: 'David Sanchez',
+      username: 'sanchez1vid',
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores quod incidunt tempore cupiditate delectus perferendis fugit culpa optio! Reiciendis qui possimus maiores magni eius, inventore ipsa soluta odit eaque ullam.'
+    },
+  ];
+
 }
