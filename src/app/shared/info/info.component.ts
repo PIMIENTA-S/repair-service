2bb724ea43e1822a1a92066e8b7ab8842fbc38ab
Angell Pimienta's <80433456+PIMIENTA-S@users.noreply.github.com>
2024-07-10 18:02:06 -0500

    feat: update style template in .ts info component

--- src/app/shared/info/info.component.ts
+++ src/app/shared/info/info.component.ts
@@ -4,7 +4,35 @@ import { icon } from './../../icons';
 @Component({
   selector: 'share-info',
   templateUrl: './info.component.html',
-  styleUrl: './info.component.css'
+  styles: `
+  .contact {
+    background: #212528;
+    color: #fff;
+    padding: 10px;
+}
+
+.msj button {
+    padding: 17px 25px;
+    font-weight: bold;
+    border-radius: 5px;
+    width: 200px;
+    transition: 0.5s;
+}
+
+.msj button:hover {
+    background-color: #fff;
+    color: #000;
+    transform: scale(1.1);
+}
+
+@media (max-width: 700px) {
+    .entire {
+        flex-direction: column;
+        gap: 15px;
+    }
+}
+
+  `
 })
 export class InfoComponent {
   icon = icon;
