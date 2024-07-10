88a0bd458b7087f615bf7a44358b2123f63d9638
Angell Pimienta's <80433456+PIMIENTA-S@users.noreply.github.com>
2024-07-10 18:06:20 -0500

    feat: remove .css and update .ts error 404 page

--- src/app/principal-page/not-found/not-found.component.ts
+++ src/app/principal-page/not-found/not-found.component.ts
@@ -4,7 +4,38 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
-  styleUrl: './not-found.component.css'
+  styles: `
+  .vh-80 {
+    height: 63vh;
+}
+
+.icon {
+    width: 80px;
+    height: 100px;
+}
+
+.face {
+    width: 80px;
+    height: 80px;
+}
+
+.person {
+    top: -40px;
+    left: 10px;
+    width: 60px;
+    height: 60px;
+}
+
+.person::before {
+    content: '';
+    width: 30px;
+    height: 10px;
+    background-color: #4a90e2;
+    border-radius: 50%;
+    position: absolute;
+}
+
+  `
 })
 export class NotFoundComponent {
   constructor(private router: Router) {}
