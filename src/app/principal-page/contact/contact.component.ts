59a66627626105b38f771bc5ac7feed5f697c7d7
Angell Pimienta's <80433456+PIMIENTA-S@users.noreply.github.com>
2024-07-12 14:50:52 -0500

    feat: delete file css for contact and update ts

--- src/app/principal-page/contact/contact.component.ts
+++ src/app/principal-page/contact/contact.component.ts
@@ -1,10 +1,14 @@
-import { icon } from './../../icons';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styles: `
+  .sub-header h1 {
+    font-size: 2.5rem;
+    font-weight: 600;
+}
+  `
 })
 export class ContactComponent {
 
