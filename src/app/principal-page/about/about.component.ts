06adeed4604dfa06c36e71f941827d09b76a4221
Angell Pimienta's <80433456+PIMIENTA-S@users.noreply.github.com>
2024-07-11 22:53:18 -0500

    feat: changes in about page and remove function in this page

--- src/app/principal-page/about/about.component.ts
+++ src/app/principal-page/about/about.component.ts
@@ -1,4 +1,3 @@
-import { icon } from './../../icons';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,15 +6,4 @@ import { Component } from '@angular/core';
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
-  icon=icon;
-
-  images = [
-    { src: 'assets/Ion/ion-1.jpg' },
-    { src: 'assets/Ion/ion-2.jpg' },
-    { src: 'assets/Ion/ion-3.jpg' },
-    { src: 'assets/Ion/ion-4.jpg' },
-    { src: 'assets/Ion/ion-5.jpg' },
-    { src: 'assets/Ion/ion-6.jpg' },
-  ];
-
 }
