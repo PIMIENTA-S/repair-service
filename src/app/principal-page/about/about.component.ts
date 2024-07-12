03c5330a7cb18bf06842e7a7f7291bae530e6457
Angell Pimienta's <80433456+PIMIENTA-S@users.noreply.github.com>
2024-07-12 14:12:05 -0500

    feat: update css file and add images in .ts for gallery

--- src/app/principal-page/about/about.component.ts
+++ src/app/principal-page/about/about.component.ts
@@ -6,4 +6,12 @@ import { Component } from '@angular/core';
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
+  images = [
+    { src: 'assets/Ion/ion-1.jpg' },
+    { src: 'assets/Ion/ion-2.jpg' },
+    { src: 'assets/Ion/ion-3.jpg' },
+    { src: 'assets/Ion/ion-4.jpg' },
+    { src: 'assets/Ion/ion-5.jpg' },
+    { src: 'assets/Ion/ion-6.jpg' },
+  ];
 }
