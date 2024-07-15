5d3c2118cfc95b336354cea75e4c5c47fd50c21c
Angell Pimienta's <80433456+PIMIENTA-S@users.noreply.github.com>
2024-07-15 17:16:57 -0500

    feat: contact bar structure added, delete files and @media screen

--- src/app/shared/contact-bar/contact-bar.component.ts
+++ src/app/shared/contact-bar/contact-bar.component.ts
@@ -1,10 +1,42 @@
+import { icon } from './../../icons';
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-contact-bar',
+  selector: 'shared-contact',
   templateUrl: './contact-bar.component.html',
-  styleUrl: './contact-bar.component.css'
+  styles: `
+  .mr-2 {
+  margin-right: 0.5rem;
+  }
+
+  .mr-3 {
+    margin-right: 1rem;
+  }
+
+  .ml-4 {
+    margin-left: 1.5rem;
+  }
+
+  .contact-info fa-icon {
+    font-size: 1.2rem;
+  }
+
+  .contact-info span {
+    font-size: 1rem;
+  }
+
+  .social-icons a {
+    font-size: 1.2rem;
+  }
+  @media (max-width: 768px) {
+    .none{
+      display: none;
+    }
+}
+
+`
 })
 export class ContactBarComponent {
+  icon = icon;
 
 }
