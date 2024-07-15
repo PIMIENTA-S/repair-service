5c8a256e888691d9ddd0dffecf00579b5b8631bb
Angell Pimienta's <80433456+PIMIENTA-S@users.noreply.github.com>
2024-07-15 15:52:34 -0500

    feat: changes in whatsapp btn for styles and function

--- src/app/shared/btn-whatsapp/btn-whatsapp.component.ts
+++ src/app/shared/btn-whatsapp/btn-whatsapp.component.ts
@@ -1,10 +1,12 @@
+import { icon } from './../../icons';
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-btn-whatsapp',
+  selector: 'share-btn',
   templateUrl: './btn-whatsapp.component.html',
   styleUrl: './btn-whatsapp.component.css'
 })
 export class BtnWhatsappComponent {
+  icon = icon;
 
 }
