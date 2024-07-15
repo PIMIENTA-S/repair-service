640b76eddf4f071b6b2abc2e8b2468f6ac685a3f
Angell Pimienta's <80433456+PIMIENTA-S@users.noreply.github.com>
2024-07-15 15:31:21 -0500

    Nuevo mensaje de commit

---
+++ src/app/shared/btn-whatsapp/btn-whatsapp.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-btn-whatsapp',
+  templateUrl: './btn-whatsapp.component.html',
+  styleUrl: './btn-whatsapp.component.css'
+})
+export class BtnWhatsappComponent {
+
+}
