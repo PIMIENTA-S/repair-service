c08ac3c8ee7540c7cec09e662854bb8e5f35c44c
Angell Pimienta's <80433456+PIMIENTA-S@users.noreply.github.com>
2024-07-17 21:21:34 -0500

    feat: forchild added in principal-page module

--- src/app/principal-page/principal-page.module.ts
+++ src/app/principal-page/principal-page.module.ts
@@ -1,19 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
 
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
-import { AppRoutingModule } from '../app-routing.module';
 import { ContactComponent } from './contact/contact.component';
-import { SharedModule } from '../shared/shared.module';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ServicesComponent } from './services/services.component';
 import { ServiceDetailComponent } from './services-detail/services-detail.component';
-import { LazyLoadDirective } from './lazy-load.directive';
+
+import { SharedModule } from '../shared/shared.module';
+import { LazyLoadDirective } from './directives/lazy-load.directive';
 
 
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'services', component: ServicesComponent },
+  { path: 'service-detail/:name', component: ServiceDetailComponent }
+];
 
 
 
@@ -25,13 +33,13 @@ import { LazyLoadDirective } from './lazy-load.directive';
     NotFoundComponent,
     ServicesComponent,
     ServiceDetailComponent,
-    LazyLoadDirective
+    LazyLoadDirective,
 
   ],
   imports: [
     CommonModule,
+    RouterModule.forChild(routes),
     FontAwesomeModule,
-    AppRoutingModule,
     SharedModule,
   ],
   exports:[
